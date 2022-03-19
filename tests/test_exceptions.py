3ae82e3f685a8872a5ed2f5f6d12534654c24463
ilindrey <scronullik@fastmail.com>
2022-03-20 06:51:40 +1100

    corrected test' exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,109 +1,96 @@
 
-import unittest
-
-import responses
-import requests
-
-from tapioca.exceptions import (
-    ClientError, ServerError, ResponseProcessException,
-    TapiocaException)
-from tapioca.tapioca import TapiocaClient
-
-from tests.client import TesterClient, TesterClientAdapter
+import pytest
+import aiohttp
 
 
-class TestTapiocaException(unittest.TestCase):
-
-    def setUp(self):
-        self.wrapper = TesterClient()
-
-    @responses.activate
-    def test_exception_contain_tapioca_client(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"data": {"key": "value"}}',
-                      status=400,
-                      content_type='application/json')
-
-        try:
-            self.wrapper.test().get()
-        except TapiocaException as e:
-            exception = e
-
-        self.assertIs(exception.client.__class__, TapiocaClient)
-
-    @responses.activate
-    def test_exception_contain_status_code(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"data": {"key": "value"}}',
-                      status=400,
-                      content_type='application/json')
-
-        try:
-            self.wrapper.test().get()
-        except TapiocaException as e:
-            exception = e
-
-        self.assertIs(exception.status_code, 400)
-
-    @responses.activate
-    def test_exception_message(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"data": {"key": "value"}}',
-                      status=400,
-                      content_type='application/json')
-
-        try:
-            self.wrapper.test().get()
-        except TapiocaException as e:
-            exception = e
-
-        self.assertEqual(str(exception), 'response status code: 400')
+from aiotapioca.exceptions import ClientError, ServerError, ResponseProcessException, TapiocaException
+from aiotapioca.tapioca import TapiocaClient
+from .client import SimpleClientAdapter
+from .fixtures import mocked, client
 
 
-class TestExceptions(unittest.TestCase):
+"""
+test TapiocaException
+"""
 
-    def setUp(self):
-        self.wrapper = TesterClient()
 
-    @responses.activate
-    def test_adapter_raises_response_process_exception_on_400s(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"erros": "Server Error"}',
-                      status=400,
-                      content_type='application/json')
+async def test_exception_contain_tapioca_tester_client(mocked, client):
+    mocked.get(client.test().data,
+               body='{"data": {"key": "value"}}',
+               status=400,
+               content_type='application/json')
+    try:
+        await client.test().get()
+    except TapiocaException as e:
+        exception = e
+    assert exception.client.__class__ is TapiocaClient
 
-        response = requests.get(self.wrapper.test().data)
 
-        with self.assertRaises(ResponseProcessException):
-            TesterClientAdapter().process_response(response)
+async def test_exception_contain_status_code(mocked, client):
+    mocked.get(client.test().data,
+               body='',
+               status=400,
+               content_type='application/json')
+    try:
+        await client.test().get()
+    except TapiocaException as e:
+        exception = e
+    assert exception.status == 400
 
-    @responses.activate
-    def test_adapter_raises_response_process_exception_on_500s(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"erros": "Server Error"}',
-                      status=500,
-                      content_type='application/json')
 
-        response = requests.get(self.wrapper.test().data)
+async def test_exception_message(mocked, client):
+    mocked.get(client.test().data,
+               body='',
+               status=400,
+               content_type='application/json')
+    try:
+        await client.test().get()
+    except TapiocaException as e:
+        exception = e
+    assert str(exception) == "response status code: 400"
 
-        with self.assertRaises(ResponseProcessException):
-            TesterClientAdapter().process_response(response)
 
-    @responses.activate
-    def test_raises_request_error(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      body='{"data": {"key": "value"}}',
-                      status=400,
-                      content_type='application/json')
+"""
+test Exceptions
+"""
 
-        with self.assertRaises(ClientError):
-            self.wrapper.test().get()
 
-    @responses.activate
-    def test_raises_server_error(self):
-        responses.add(responses.GET, self.wrapper.test().data,
-                      status=500,
-                      content_type='application/json')
+async def test_adapter_raises_response_process_exception_on_400s(mocked, client):
+    mocked.get(client.test().data,
+               body='{"errors": "Server Error"}',
+               status=400,
+               content_type='application/json')
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(client.test().data)
+    with pytest.raises(ResponseProcessException):
+        await SimpleClientAdapter().process_response(response)
 
-        with self.assertRaises(ServerError):
-            self.wrapper.test().get()
+
+async def test_adapter_raises_response_process_exception_on_500s(mocked, client):
+    mocked.get(client.test().data,
+               body='{"errors": "Server Error"}',
+               status=500,
+               content_type='application/json')
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(client.test().data)
+    with pytest.raises(ResponseProcessException):
+        await SimpleClientAdapter().process_response(response)
+
+
+async def test_raises_request_error(mocked, client):
+    mocked.get(client.test().data,
+               body='{"data": {"key": "value"}}',
+               status=400,
+               content_type='application/json')
+
+    with pytest.raises(ClientError):
+        await client.test().get()
+
+
+async def test_raises_server_error(mocked, client):
+    mocked.get(client.test().data,
+               status=500,
+               content_type='application/json')
+    with pytest.raises(ServerError):
+        await client.test().get()
+
