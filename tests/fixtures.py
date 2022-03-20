ead9dcf23e1183c1af8db5b775641592f78a1c02
ilindrey <scronullik@fastmail.com>
2022-03-20 11:31:51 +1100

    corrected test' tapioca

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,7 +4,7 @@ import pytest_asyncio
 from aioresponses import aioresponses
 
 from aiotapioca.serializers import SimpleSerializer
-from .client import SimpleClient, SerializerClient
+from .client import SimpleClient, SerializerClient, TokenRefreshClient, XMLClient
 
 
 @pytest_asyncio.fixture
@@ -22,6 +22,18 @@ async def client_serializer_class():
 @pytest_asyncio.fixture
 async def serializer_client():
     async with SerializerClient() as c:
+        yield c
+
+
+@pytest_asyncio.fixture
+async def xml_client():
+    async with XMLClient() as c:
+        yield c
+
+
+@pytest_asyncio.fixture
+async def token_refresh_client():
+    async with TokenRefreshClient(token='token', refresh_token_by_default=True) as c:
         yield c
 
 
