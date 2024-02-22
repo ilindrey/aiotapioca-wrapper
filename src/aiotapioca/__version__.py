e1cd0942708d2d06a729c41c1d798ccc56d8b7a3
ilindrey <scronullik@fastmail.com>
2024-02-22 14:15:23 +1100

    fix __version__

--- src/aiotapioca/__version__.py
+++ src/aiotapioca/__version__.py
@@ -1,18 +1,13 @@
 from __future__ import annotations
 
-import sys
+import importlib.metadata as importlib_metadata
 
-from packaging.version import LegacyVersion, Version, parse
+from packaging.version import Version, parse
 
-
-if sys.version_info >= (3, 8):
-    import importlib.metadata as importlib_metadata
-else:
-    import importlib_metadata  # type: ignore
 
 try:
     __version__ = importlib_metadata.version(__package__)
-    parsed_version: LegacyVersion | Version | None = parse(__version__)
+    parsed_version: Version | None = parse(__version__)
 except importlib_metadata.PackageNotFoundError:
     __version__ = "UNKNOWN"
     parsed_version = None
